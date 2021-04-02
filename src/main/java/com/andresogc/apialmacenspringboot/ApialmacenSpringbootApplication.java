94ebeb8b9a6499c6fdbb55c424a12f514fea5206
andresogc <andresogc2011@gmail.com>
2021-04-02 11:11:19 -0500

    first commit

---
+++ src/main/java/com/andresogc/apialmacenspringboot/ApialmacenSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.andresogc.apialmacenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApialmacenSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApialmacenSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andresogc/apialmacenspringboot/ApialmacenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresogc.apialmacenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialmacenSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
